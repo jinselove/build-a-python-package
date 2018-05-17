1fed9512f95d9a2b588ddc90b6932c9fab5230f4
jinselove <jyli@uchicago.edu>
2018-05-17 14:18:05 -0500

    follow https://python-packaging.readthedocs.io/en/latest/minimal.html to build the first package

---
+++ funnist/README.md
@@ -0,0 +1,7 @@
+## how to install
+
+### how to install locally
+  
+    * $ pip install .
+
+    * or $ pip install -e . (install the package with a symlink so that changes to the source files will be immediately available to other users of the package on our system)
---
+++ funnist/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://example',
+      author='Flying circus',
+      author_email='email@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_sage=False)
